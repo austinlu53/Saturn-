6a9ea1315a5760ff88d83687759de3eb014b68ff
austinlu53 <austinl53@nycstudents.net>
2026-04-27 10:14:47 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         try {
-            File myFile = new File("src/classes");
-            Scanner reader = new Scanner(myFile);
-            while (reader.hasNextLine()) {
-                String data = reader.nextLine();
-                String[] strs = data.split(",");
-                System.out.println("INSERT INTO Courses (DepartmentID,CourseType,CourseName) VALUES (10,'" + strs[1] + "','" + strs[0] + "');");
+            File StudentFile = new File("src/students.txt");
+            Scanner scan = new Scanner(StudentFile);
+            while(scan.hasNextLine()){
+                String student_firstname = scan.next();
+                String student_lastname = scan.next();
+                System.out.println("INSERT INTO Students (StudentFirstName,StudentLastName) " +
+                        "VALUES ("+"'"+student_firstname+"'"+","+"'"+student_lastname+"'"+");");
             }
-            reader.close();
+            scan.close();
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
+
     }
 }
