9f4f254424020c7f0269b4c2c01e42312a6529fc
EdwardChen <edwardc188@nycstudents.net>
2026-05-05 10:47:39 -0400

    using Class object

--- src/Class.java
+++ src/Class.java
@@ -10,6 +10,7 @@ public class Class {
     private int classID = 0;
     private static int classIDtrack = 0;
     private static ArrayList<Class> classesList;
+    public static boolean generated = false;
 
     public Class(int courseID, int period, int roomID) {
         this.courseID = courseID;
@@ -24,32 +25,34 @@ public class Class {
         return "INSERT INTO Classes (CourseID,PeriodNumber,RoomID) VALUES (" + courseID + "," + period + "," + roomID + ");";
     }
     public static void generateClasses() {
-        ArrayList<Class> classList = new ArrayList<>();
-        boolean[][] classes = new boolean[720][10];
-        int count = 0;
-        try {
-            File myFile = new File("src/CourseID");
-            Scanner reader = new Scanner(myFile);
-            while (reader.hasNextLine()) {
-                int data = reader.nextInt();
-                for (int i = 0; i < (int) (Math.random() * 5) + 1; i++) {
-                    int ClassID = (int) (Math.random()*720) + 1;
-                    int PeriodNum = (int) (Math.random()*10) + 1;
-                    while (classes[ClassID - 1][PeriodNum - 1]) {
-                        ClassID = (int) (Math.random()*720) + 1;
-                        PeriodNum = (int) (Math.random()*10) + 1;
+        if (!generated) {
+            ArrayList<Class> classList = new ArrayList<>();
+            boolean[][] classes = new boolean[720][10];
+            int count = 0;
+            try {
+                File myFile = new File("src/CourseID");
+                Scanner reader = new Scanner(myFile);
+                while (reader.hasNextLine()) {
+                    int data = reader.nextInt();
+                    for (int i = 0; i < (int) (Math.random() * 5) + 1; i++) {
+                        int ClassID = (int) (Math.random()*720) + 1;
+                        int PeriodNum = (int) (Math.random()*10) + 1;
+                        while (classes[ClassID - 1][PeriodNum - 1]) {
+                            ClassID = (int) (Math.random()*720) + 1;
+                            PeriodNum = (int) (Math.random()*10) + 1;
+                        }
+                        classList.add(new Class(data,PeriodNum,ClassID));
+                        count++;
+                        classes[ClassID - 1][PeriodNum - 1] = true;
                     }
-                    classList.add(new Class(data,PeriodNum,ClassID));
-                    count++;
-                    classes[ClassID - 1][PeriodNum - 1] = true;
-                }
 
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
             }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
+            classesList = classList;
         }
-        classesList = classList;
     }
 
     public int getClassID() {
