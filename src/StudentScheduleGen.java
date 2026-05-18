e973ff18cee62848a4a03006a0ecb51f8c00353e
amonochrome <otheredwardchen0200@gmail.com>
2026-05-18 19:08:24 -0400

    schedule fix

--- src/StudentScheduleGen.java
+++ src/StudentScheduleGen.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 
 public class StudentScheduleGen {
     public static void main(String[] args) {
+        ClassA.generateClasses();
         ArrayList<ClassA> classes = ClassA.getClassesList();
-        for (ClassA c : classes) System.out.println(c);
-        ArrayList<Student> students = Student.generateStudents();
+        // for (ClassA c : classes) { System.out.println(c);};
+        ArrayList<Student> students = Student.getStudents();
         ArrayList<StudentClasses> studentClasses = new ArrayList<>();
         for (Student student : students) {
             for (int j = 1; j <= 10; j++) {
@@ -14,16 +15,26 @@ public class StudentScheduleGen {
                     c = classes.get((int) (Math.random()*classes.size()));
                 }
                 studentClasses.add(new StudentClasses(student.getStudentID(),c.getClassID()));
+
             }
         }
         int[] i = new int[classes.size()];
-        int[] j = new int[students.size()];
+        int[] j = new int[studentClasses.size()];
         for (StudentClasses s : studentClasses) {
             //System.out.println(s);
             i[s.getClassID()-1]++;
             j[s.getStudentID()-1]++;
         }
-        System.out.println(Arrays.toString(i));
-        System.out.println(Arrays.toString(j));
+        // System.out.println(Arrays.toString(i));
+        // System.out.println(Arrays.toString(j));
+        System.out.print("INSERT INTO StudentClasses (StudentID,ClassID) VALUES ");
+        for (int n = 0; n < studentClasses.size(); n++) {
+            StudentClasses s = studentClasses.get(n);
+            System.out.print("(" + s.getStudentID() + "," + s.getClassID() + ")");
+            if (n < studentClasses.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print(";");
     }
 }
